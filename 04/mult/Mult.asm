e5a40040717c2c3ca70e821e3f9db218736fb82c
Juan Pablo <juantkd99@gmail.com>
2021-04-19 16:55:20 -0500

    Finalización proyecto 4

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -1,15 +1,11 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
+// This file is part of http://www.nand2tetris.org
+// and the book “The Elements of Computing Systems”
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Mult.asm
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-//
-// This program only needs to handle arguments that satisfy
-// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
 @sum
 M=0     // Initialize sum to 0
 @R1
